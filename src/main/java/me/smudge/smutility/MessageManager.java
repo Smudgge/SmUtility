fed9be87ac3f9eedea70c46605fdb1bbbd423e42
smuddgge <68658429+smuddgge@users.noreply.github.com>
2022-09-30 17:24:50 +0100

    Remove late version error specifics

--- src/main/java/me/smudge/smutility/MessageManager.java
+++ src/main/java/me/smudge/smutility/MessageManager.java
@@ -32,14 +32,6 @@ public class MessageManager {
 
         if (message == null) {
             SmUtility.getLogger().warn("Message is null! Please check messages.yml config for missing values.");
-            SmUtility.getLogger().warn(
-                    "If updating to 1.0.4 it may had been because this is missing :\n" +
-                            "  # Command messages\n" +
-                            "  reloaded: '{prefix} Reloaded all configs! <3'\n" +
-                            "  playerIsOffline: '{prefix} Player is offline'\n" +
-                            "  requiresArguments: '&7&l> &7This command requires arguments. /<commands> <args>'\n" +
-                            "  ^^^^^^^"
-            );
             message = "null";
         }
 
